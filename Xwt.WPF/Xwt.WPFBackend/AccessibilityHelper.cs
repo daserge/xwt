f5df074a782c6b0e091eb7cdd499238f3c97a1bf
Sergey Shakhnazarov <v-seshak@microsoft.com>
2019-02-05 15:09:20 +0300

    [Mac][Wpf] AccessibilityHelper Allows to check if accessibility (VO/Narrator) is in use

---
+++ Xwt.WPF/Xwt.WPFBackend/AccessibilityHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Automation.Peers;
+
+namespace Xwt.WPF.Xwt.WPFBackend
+{
+	public sealed class AccessibilityHelper : Accessibility.BaseAccessibilityHelper
+	{
+		[DllImport("user32.dll")]
+		static extern bool SystemParametersInfo (int iAction, int iParam, out bool bActive, int iUpdate);
+
+		static bool Is3rdPartyScreenReaderActive ()
+		{
+			int iAction = 70; // SPI_GETSCREENREADER constant;
+			int iParam = 0;
+			int iUpdate = 0;
+			bool bActive = false;
+			bool bReturn = SystemParametersInfo (iAction, iParam, out bActive, iUpdate);
+			return bReturn && bActive;
+		}
+
+		static bool IsSystemNarratorActive {
+			get {
+				return AutomationPeer.ListenerExists (AutomationEvents.AutomationFocusChanged);
+			}
+		}
+
+		public override bool AccessibilityInUse {
+			get {
+				return IsSystemNarratorActive || Is3rdPartyScreenReaderActive ();
+			}
+		}
+
+		// AccessibilityInUseChanged event would require listening for a top-level window' events in WndProc
+	}
+}
---
+++ Xwt/Xwt.Accessibility/BaseAccessibilityHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Xwt.Accessibility
+{
+	public class BaseAccessibilityHelper
+	{
+		public virtual bool AccessibilityInUse { get; } = false;
+		public event EventHandler AccessibilityInUseChanged;
+		protected virtual void OnAccessibilityInUseChanged ()
+		{
+			AccessibilityInUseChanged?.Invoke (this, EventArgs.Empty);
+		}
+
+		// TODO:
+		public virtual void MakeAccessibilityAnnouncement () { }
+	}
+}
